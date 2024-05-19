60a0f8d593556825c19d504cb0711c3d34d0732b
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-05-19 22:27:18 +0700

    feat: added animations using GSAP

--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -1,11 +1,63 @@
+import { useRef } from "react";
 import VisionPatterns from "./designs/Vision";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Vision = () => {
+  const visionRef = useRef(null);
+  const visionText = useRef(null);
+  const missionText = useRef(null);
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: visionRef.current,
+          start: "20% center",
+        },
+      });
+      tl.fromTo(
+        visionRef.current,
+        { scaleX: 0 },
+        {
+          scaleX: 1,
+          stagger: 1,
+          duration: 1,
+          ease: "circ.out",
+          transformOrigin: "left",
+        }
+      );
+      tl.fromTo(
+        visionText.current.children,
+        { opacity: 0, x: -500 },
+        { opacity: 1, x: 0, stagger: 0.2, duration: 1, ease: "back.out" }
+      );
+
+      tl.fromTo(
+        missionText.current.children,
+        { opacity: 0, x: 500 },
+        { opacity: 1, x: 0, stagger: 0.2, duration: 1, ease: "back.out" }
+      );
+
+      tl.fromTo(
+        ".absolute",
+        { opacity: 0, scale: 0 },
+        {
+          opacity: 1,
+          scale: 1,
+          duration: 1,
+          ease: "power1.in",
+          transformOrigin: "center",
+        }
+      );
+    },
+    { scope: visionRef }
+  );
   return (
-    <section className="relative bg-coral padding">
+    <section ref={visionRef} className="relative bg-coral padding">
       <VisionPatterns />
       <div className="flex justify-between items-start gap-8 max-lg:flex-col">
-        <article className="max-w-lg">
+        <article ref={visionText} className="max-w-lg">
           <h1 className="title mb-4">Visi</h1>
           <p className="font-gotham text-primary">
             Menjadi pilihan utama konsumen untuk makanan cepat saji berkualitas
@@ -13,7 +65,7 @@ const Vision = () => {
             setiap gigitannya{" "}
           </p>
         </article>
-        <article className="max-w-lg">
+        <article ref={missionText} className="max-w-lg">
           <h1 className="title mb-4">Misi</h1>
           <ul className="font-gotham text-primary list-disc">
             <li>
